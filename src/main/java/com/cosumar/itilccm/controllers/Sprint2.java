5b7bef85a80e2d2cdc122a88ca1eb45b6ff00bf1
ayoubrh <rhnabri.ayoub@gmail.com>
2016-04-11 18:14:55 +0200

    sprint 2 debut

---
+++ src/main/java/com/cosumar/itilccm/controllers/Sprint2.java
@@ -0,0 +1,34 @@
+package com.cosumar.itilccm.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cosumar.itilccm.entities.User;
+import com.cosumar.itilccm.metier.AdminMetier;
+import com.cosumar.itilccm.metier.UtilisateurMetier;
+
+@Controller
+@RequestMapping(value="/config")
+public class Sprint2 {
+	
+	@Autowired
+	private AdminMetier m;
+	
+	@Autowired
+	private UtilisateurMetier mu;
+	
+	@RequestMapping(value="/admin/dashbord")
+	public String index(Model model){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    String logged_m = auth.getName();
+	    System.out.println(logged_m);
+	    User logged = mu.getUserByMatricule(logged_m);
+	    System.out.println(logged.getNom());
+		model.addAttribute("logged", logged);
+		return "sprint2/dashbord";
+	}
+}
